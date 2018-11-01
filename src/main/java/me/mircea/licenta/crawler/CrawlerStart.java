5efcb0ae6fbbfe37125897dda78e47eaae874931
mcdobr <mircea.dobreanu@gmail.com>
2018-11-01 15:34:51 +0200

    Started transitioning to Datastore.

--- src/main/java/me/mircea/licenta/crawler/CrawlerStart.java
+++ src/main/java/me/mircea/licenta/crawler/CrawlerStart.java
@@ -12,12 +12,41 @@ import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.ObjectifyFactory;
+import com.googlecode.objectify.ObjectifyService;
+import com.googlecode.objectify.Work;
+
+import me.mircea.licenta.core.entities.Book;
+
+
 public final class CrawlerStart {
 	private static final Logger logger = LoggerFactory.getLogger(CrawlerStart.class);
 	
 	public static final ExecutorService executor = Executors.newFixedThreadPool(2 * Runtime.getRuntime().availableProcessors());
 	
 	public static void main(String[] args) throws InterruptedException {
+		Datastore datastore = DatastoreOptions.newBuilder()
+				.setProjectId("bookworm-221210")
+				.build()
+				.getService();
+		ObjectifyService.init(new ObjectifyFactory(datastore));
+		ObjectifyService.register(Book.class);
+		
+		Book book = new Book();
+		book.setTitle("Anna Karenina");
+		book.setIsbn("9781234567890");
+		book.setAuthors(Arrays.asList("Lev Tolstoi"));
+		
+		ObjectifyService.run(new Work<Key<Book>>() {
+			@Override
+			public Key<Book> run() {
+				return ObjectifyService.ofy().save().entity(book).now();
+			}
+		});
+		
 		List<String> seedList = Arrays.asList(
 				"https://carturesti.ro/raft/carte-109?per-page=90",
 				"http://www.librariilealexandria.ro/carte",
